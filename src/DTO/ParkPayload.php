97f703c5174b6dd8c52bc33f33d8a9d934cfb3ab
Tonko Mulder <tonko@tonkomulder.nl>
2026-08-11 15:27:20 +0200

    ParkPayload met validatieconstraints voor POST-endpoint

--- src/DTO/ParkPayload.php
+++ src/DTO/ParkPayload.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
+use App\Validator\UniquePark;
 use Symfony\Component\Validator\Constraints as Assert;
 
-final readonly class Park
+final readonly class ParkPayload
 {
     public function __construct(
         #[Assert\NotBlank()]
         #[Assert\Length(min: 3, max: 255)]
+        #[UniquePark()]
         public string $name,
         public bool $hasPool = false,
     ) {
