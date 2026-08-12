6149d3ce3d29d0524714e1f6fc7d02a1f8e419e7
Tonko Mulder <tonko@tonkomulder.nl>
2026-08-12 22:15:19 +0200

    POST-endpoint met MapRequestPayload, 201 met Location, tests voor 400/415/422

--- src/Controller/ParkController.php
+++ src/Controller/ParkController.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\ParkPayload;
 use App\Entity\Park;
+use App\Factory\ParkFactory;
 use App\Repository\ParkRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ParkController extends AbstractController
 {
     public function __construct(
         private readonly ParkRepository $repository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ParkFactory $parkFactory,
     ) {
     }
 
@@ -32,6 +38,22 @@ final class ParkController extends AbstractController
     {
         return $this->render('park/show.html.twig', [
             'park' => $park,
+        ]);
+    }
+
+    #[Route('/parks', name: 'park_store', methods: ['POST'])]
+    public function store(#[MapRequestPayload] ParkPayload $payload): Response
+    {
+        $park = $this->parkFactory->create(
+            name: $payload->name,
+            hasPool: $payload->hasPool,
+        );
+
+        $this->entityManager->persist($park);
+        $this->entityManager->flush();
+
+        return $this->json($park, status: Response::HTTP_CREATED, headers: [
+            'Location' => $this->generateUrl('park_show', ['slug' => $park->getSlug()]),
         ]);
     }
 }
--- tests/ParkControllerTest.php
+++ tests/ParkControllerTest.php
@@ -4,17 +4,25 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Entity\Park;
 use App\Factory\ParkFactory;
+use App\Repository\ParkRepository;
+use App\Validator\UniquePark;
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class ParkControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
     private EntityManagerInterface $entityManager;
     private ParkFactory $factory;
+    private ParkRepository $repository;
 
     protected function setUp(): void
     {
@@ -24,6 +32,7 @@ final class ParkControllerTest extends WebTestCase
 
         $this->entityManager = static::getContainer()->get(EntityManagerInterface::class);
         $this->factory = static::getContainer()->get(ParkFactory::class);
+        $this->repository = $this->entityManager->getRepository(Park::class);
     }
 
     #[Test]
@@ -46,5 +55,127 @@ final class ParkControllerTest extends WebTestCase
         $this->client->request('GET', '/parks/witterzomer');
 
         self::assertResponseStatusCodeSame(404, 'slug does not exist');
+    }
+
+    #[Test]
+    public function canCreateNewPark(): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => 'Witterzomer',
+        ]);
+
+        self::assertResponseStatusCodeSame(201);
+        self::assertSame(1, $this->repository->count(['slug' => 'witterzomer']));
+        self::assertResponseHeaderSame('Location', '/parks/witterzomer');
+    }
+
+    #[Test]
+    public function canCreateParkWithPool(): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => 'Witterzomer',
+            'hasPool' => true,
+        ]);
+        self::assertResponseStatusCodeSame(201);
+        $park = $this->repository->findOneBy(['slug' => 'witterzomer']);
+
+        self::assertNotNull($park);
+        self::assertSame('Witterzomer', $park->getName());
+        self::assertTrue($park->hasPool());
+    }
+
+    #[Test]
+    public function canNotCreateParkWithInvalidInputForHavingAnPool(): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => 'Witterzomer',
+            'hasPool' => 'ja',
+        ]);
+        self::assertResponseIsUnprocessable();
+        $errors = json_decode((string) $this->client->getResponse()->getContent(), flags: JSON_THROW_ON_ERROR);
+
+        self::assertSame(
+            'This value should be of type {{ type }}.', $errors->violations[0]->template
+        );
+    }
+
+    #[Test]
+    public function canNotCreateDuplicateParks(): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => 'Witterzomer',
+        ]);
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => 'Witterzomer',
+        ]);
+
+        self::assertResponseIsUnprocessable();
+
+        $errors = json_decode((string) $this->client->getResponse()->getContent(), flags: JSON_THROW_ON_ERROR);
+
+        self::assertSame('urn:uuid:' . UniquePark::PARK_NOT_UNIQUE_ERROR, $errors->violations[0]->type);
+
+        self::assertSame(1, $this->repository->count());
+    }
+
+    public static function generateTooShortOrBlankParkName(): \Generator
+    {
+        yield 'blank park name' => ['', NotBlank::IS_BLANK_ERROR];
+        yield 'not empty park name' => ['W', Length::TOO_SHORT_ERROR];
+        yield 'park name is too long' => [str_repeat('Witterzomer', 100), Length::TOO_LONG_ERROR];
+    }
+
+    #[Test]
+    #[DataProvider('generateTooShortOrBlankParkName')]
+    public function shouldHaveParkNameWithReasonableLength(string $value, string $uuid): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => $value,
+        ]);
+
+        self::assertResponseIsUnprocessable();
+
+        $errors = json_decode((string) $this->client->getResponse()->getContent(), associative: true, flags: JSON_THROW_ON_ERROR);
+
+        $violation = array_filter($errors['violations'], fn ($x) => $x['type'] === 'urn:uuid:' . $uuid);
+        self::assertCount(1, $violation);
+    }
+
+    #[Test]
+    public function throwsTypeErrorWithNullValue(): void
+    {
+        $this->client->jsonRequest('POST', '/parks', [
+            'name' => null,
+        ]);
+
+        self::assertResponseIsUnprocessable();
+
+        $errors = json_decode((string) $this->client->getResponse()->getContent(), flags: JSON_THROW_ON_ERROR);
+
+        self::assertSame(
+            'This value should be of type {{ type }}.', $errors->violations[0]->template
+        );
+    }
+
+    #[Test]
+    public function throwsUnsupportedMediaTypeWithUnknownContentType(): void
+    {
+        $this->client->request('POST', '/parks', [
+            'name' => 'Witterzomer',
+        ], server: ['CONTENT_TYPE' => 'application/some-random-type']);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    #[Test]
+    public function throwsBadRequestErrorWhenInvalidJsonIsGiven(): void
+    {
+        $server = [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_ACCEPT' => 'application/json',
+        ];
+        $this->client->request('POST', '/parks', server: $server, content: '{"name": "}');
+
+        self::assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
     }
 }
